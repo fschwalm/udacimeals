805ed4a173d875f75dc16dc135a763941d47c50d
Fagner <fagnerschwalm@gmail.com>
2018-01-10 16:18:14 -0200

    feat: Use Connect to connect the Store on App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../../actions';
 
 class App extends Component {
   
   render() {
+    console.log('Props', this.props);
+    
     return (
       <div>
         <h1>Hello World</h1>
@@ -11,4 +15,28 @@ class App extends Component {
   }
 }
 
-export default App
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
